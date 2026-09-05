087a43db421a8842c41fa152cf53502ccbfef092
periyasamymuthusamy442 <periyasamymuthusamy442@gmail.com>
2026-09-05 07:31:30 +0530

    version7

--- app.py
+++ app.py
@@ -4118,11 +4118,9 @@ def internal_server_error(error):
 # ============================================================
 # RUN
 # ============================================================
-
 if __name__ == "__main__":
-
+    import os
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
     )
